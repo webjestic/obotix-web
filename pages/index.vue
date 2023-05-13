a76e1570e176e902fdcbb62e46ae80f446d7e167
Michael N <michael.newlon@tn.gov>
2023-05-13 10:43:53 -0400

    i14 | adding secure page and deep links

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
     <h1>obotix.one</h1>
 
     <p>Secure Channel 
-        <a href="https://obotix.one/termsofservice/">Terms of Service</a> and 
-        <a href="https://obotix.one/privacypolicy/">Privacy Policy</a>
+        <a href="https://obotix.one/securechannel/termsofservice/">Terms of Service</a> and 
+        <a href="https://obotix.one/securechannel/privacypolicy/">Privacy Policy</a>
     </p>
 </template>
 
--- pages/securechannel/index.vue
+++ pages/securechannel/index.vue
@@ -2,10 +2,10 @@
     <h1>Secure Channel</h1>
     <h3>Alexa Skill by obotix.one</h3>
 
-        <p>Secure Channel
-            <a href="https://obotix.one/termsofservice/">Terms of Service</a> and
-            <a href="https://obotix.one/privacypolicy/">Privacy Policy</a>
-        </p>
+    <p>Secure Channel
+        <a href="https://obotix.one/securechannel/termsofservice/">Terms of Service</a> and
+        <a href="https://obotix.one/securechannel/privacypolicy/">Privacy Policy</a>
+    </p>
 </template>
 
 <script setup>
