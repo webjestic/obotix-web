a871dca5b6ad597f200e4776608431205523f318
Michael N <michael.newlon@tn.gov>
2023-05-13 10:19:11 -0400

    skill pages for policy

---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>obotix.one</h1>
+</template>
---
+++ pages/securechannel/termsofservice.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>Terms of Service</h1>
+    <h2>Secure Channel</h2>
+    <h3>Alexa Skill by obotix.one</h3>
+</template>
