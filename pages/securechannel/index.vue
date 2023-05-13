5b7f7ffc46afce961a4192dba4d793a95d2e8d30
Michael N <37873018+webjestic@users.noreply.github.com>
2023-05-13 11:29:18 -0400

    14 link to tos and privacy pages (#16)
    * i14 | adding secure page and deep links
    
    * i14 | adding secure page and deep links
    
    * i14 | remove dockerfile
    
    * i14 | remove dockerfile
    
    ---------
    
    Co-authored-by: Michael N <michael.newlon@tn.gov>

--- pages/securechannel/index.vue
+++ pages/securechannel/index.vue
@@ -2,10 +2,7 @@
     <h1>Secure Channel</h1>
     <h3>Alexa Skill by obotix.one</h3>
 
-    <p>Secure Channel
-        <a href="https://obotix.one/securechannel/termsofservice/">Terms of Service</a> and
-        <a href="https://obotix.one/securechannel/privacypolicy/">Privacy Policy</a>
-    </p>
+    <p><a href="/">Home</a> | <a href="#">Secure Channel</a> | <a href="termsofservice/">Terms of Service</a> | <a href="privacypolicy/">Privacy Policy</a></p>
 </template>
 
 <script setup>
