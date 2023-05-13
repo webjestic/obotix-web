099d6bb8769c37f8c995f133de9ced1b88c9c1ea
Michael N <michael.newlon@tn.gov>
2023-05-13 11:27:34 -0400

    i14 | remove dockerfile

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,7 @@
 <template>
     <h1>obotix.one</h1>
 
-    <p>Secure Channel 
-        <a href="https://obotix.one/securechannel/termsofservice/">Terms of Service</a> and 
-        <a href="https://obotix.one/securechannel/privacypolicy/">Privacy Policy</a>
-    </p>
+    <p><a href="/">Home</a> | <a href="securechannel/">Secure Channel</a> | <a href="securechannel/termsofservice/">Terms of Service</a> | <a href="securechannel/privacypolicy/">Privacy Policy</a></p>
 </template>
 
 <script setup>
